8a5ecda226f0ea914cf8d2a6196e65207438dcb8
osprey0222 <smartunity0222@gmail.com>
2022-05-20 20:32:04 +0000

    adding template navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="{{ mix('/js/app.js') }}"></script>
+    <title>Tamworth Spartan Volleyball Club</title>
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
+
 <body>
     <div id="root"></div>
+
 </body>
+
 </html>
