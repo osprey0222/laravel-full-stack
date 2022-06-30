5c6a69fb022f8548943444ec172f79c63905cc28
osprey0222 <smartunity0222@gmail.com>
2022-06-30 18:56:12 +0000

    [FEAT] #9 background CSS fixed

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -3,7 +3,13 @@ import React from "react"
 const Home = () => {
     return (
         <div className="bg-gray-800">
-            <p>Home</p>
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+                <h3>About</h3>
+                {/* lorem ipsum with white text */}
+                <div className="text-white text-center md:text-left">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum quas illum cum ipsa aliquam nam iure provident magnam perferendis impedit voluptas, nesciunt laudantium corrupti quibusdam minima iste quaerat? Nam!</p>
+                </div>
+            </div>
         </div>
     )
 }
